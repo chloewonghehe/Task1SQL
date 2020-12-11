422b22eb142182a0330e53b156d90523c275d8a9
chloewonghehe <63860057+chloewonghehe@users.noreply.github.com>
2020-12-11 16:09:36 +0000

    Update Task 1 SSRS.sql

--- Task 1 SSRS.sql
+++ Task 1 SSRS.sql
@@ -1,8 +1,5 @@
 
 
-
-
-
 SELECT 
 O.Id, TPF.Name AS [RentalPayment/Frequency], TP.PaymentAmount As [RetalPayment],
 
@@ -13,14 +10,13 @@ O.Id, TPF.Name AS [RentalPayment/Frequency], TP.PaymentAmount As [RetalPayment],
 
  FROM Keys.dbo.OwnerProperty AS OP
  LEFT JOIN dbo.Owners AS O ON OP.OwnerId = O.Id
- LEFT JOIN Keys.dbo.OwnershipStatus AS OS ON OP.OwnershipStatusId = OS.Id 
- LEFT JOIN Keys.dbo.Property AS P ON OP.PropertyId =P.ID
- LEFT JOIN Keys.dbo.Address AS A ON  P.AddressId = A.AddressId
-
+ LEFT JOIN dbo.OwnershipStatus AS OS ON OP.OwnershipStatusId = OS.Id 
+ LEFT JOIN dbo.Property AS P ON OP.PropertyId =P.ID
+ LEFT JOIN dbo.Address AS A ON  P.AddressId = A.AddressId
  LEFT JOIN dbo.Person AS PER ON A.AddressId = PER.PhysicalAddressId 
  LEFT JOIN dbo.PropertyExpense AS PE ON P.Id = PE.PropertyId
- LEFT JOIN Keys.dbo.TenantProperty AS TP ON TP.PropertyId = P.Id
- LEFT JOIN Keys.dbo.TenantPaymentFrequencies AS TPF ON TPF.ID = TP.PaymentFrequencyId
+ LEFT JOIN dbo.TenantProperty AS TP ON TP.PropertyId = P.Id
+ LEFT JOIN dbo.TenantPaymentFrequencies AS TPF ON TPF.ID = TP.PaymentFrequencyId
 
 
  WHERE P.Name ='Property A' AND OS.Name = 'owner' ;
